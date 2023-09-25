72d87bda7a1f94f8f0bd0eeb3d4440042fe3b737
samarthbhat <samarthbhat@utexas.edu>
2023-09-24 21:07:03 -0500

    added static search bar to navigation - related to #23

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function NavBar() {
   return (
@@ -48,6 +50,12 @@ function NavBar() {
               </Link>
             </Nav.Link>
           </Nav>
+          <Form className="d-flex justify-content-end">
+            <Form.Control type="search" id="searchText" placeholder="Search..." 
+                className="mx-2" aria-label="Search"></Form.Control>
+            <Button type="submit" variant="dark">Search</Button>
+          </Form>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
