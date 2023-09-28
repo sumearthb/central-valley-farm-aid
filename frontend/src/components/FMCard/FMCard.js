ee66d77097afbf58ff324e536ac3e484bb335f47
Samarth Bhat <samarthbhat@utexas.edu>
2023-09-27 21:20:42 -0500

    added farmers markets card - related to #18, #19

--- frontend/src/components/FMCard/FMCard.js
+++ frontend/src/components/FMCard/FMCard.js
@@ -3,15 +3,18 @@ import "./FMCard.css";
 
 // may have to import proptypes later?
 
-const FMCard = ({ title, image, description }) => {
+const FMCard = ({ title, image, location, site, hours, seasons, vendors }) => {
     return (
       // specifies CSS class used
-      <div className="fm-card">
-        <img src={image} alt={title} className="fm-card-image" />
+      <div className="fm-card mb-4" style={{position: "relative" }}>
+        <img src={image} alt={title} className="fm-card-image mx-auto" />
         <div className="fm-card-details">
-          <h2 className="fm-card-title">{title}</h2>
-          <p className="fm-card-description">{description}</p>
-          {/* Add more details if needed */}
+          <h2 className="fm-card-title" style={{ marginBottom: "10px" }}>{title}</h2>
+          <p className="fm-card-location">{location}</p>
+          <p className="fm-card-site">{site}</p>
+          <p className="fm-card-hours">{hours}</p>
+          <p className="fm-card-seasons">{seasons}</p>
+          <p className="fm-card-vendors">{vendors}</p>
         </div>
       </div>
     );
--- frontend/src/pages/FarmersMarkets.js
+++ frontend/src/pages/FarmersMarkets.js
@@ -4,17 +4,50 @@ import { Link } from "react-router-dom";
 import fairfax from "../card-pics/markets-pics/fairfaxcommunity.png";
 import springs from "../card-pics/markets-pics/springscommunity.png";
 import novato from "../card-pics/markets-pics/downtownnovato.png";
+import { Container, Col } from "react-bootstrap";
 
-const FarmersMarkets = () => {
-  // Hard-coded data
-  const FMData = [
-    { title: "Fairfax Community Farmers' Market", image: fairfax, description: "" },
-    { title: "The Springs Community Farmers' Market", image: springs, description: "Description 2" },
-    { title: "Downtown Novato Community Farmers' Market", image: novato, description: "Description 3" }
-  ];
+const FMData = [
+  { title: "Fairfax Community Farmers' Market", image: fairfax, location: "Location: 142 Bolinas Road, Fairfax, California 94930, USA", hours: "Hours: Wednesday: 04:00 PM - 08:00 PM", seasons: "Seasons: May to October", vendors: "# of vendors: 35" },
+  { title: "The Springs Community Farmers' Market", image: springs, location: "Location: Parking lot at Boyes Blvd & Hwy 12, Boyes Hot Springs , California 95476", hours: "Hours: N/A", seasons: "Seasons: September to November", vendors: "# of vendors: 20" },
+  { title: "Downtown Novato Community Farmers' Market", image: novato, location: "Location: 7th Street behind the CVS between Grant Ave & Novato Blvd , Novato , California 94947", hours: "Hours: Tuesday: 04:00 PM - 08:00 PM", seasons: "Seasons: May to September", vendors: "# of vendors: 45" }
+];
+
+const FMGrid = () => {
 
   return (
-    <div className="farmersmarkets">
+    <Container className="d-flex justify-content-center flex-column">
+      <Container className="container text-center mt-5 mb-4">
+        <h1>Farmers' Markets</h1>
+      </Container>
+
+      <Container className="px-4">
+        <Container className="row gx-3">
+          {FMData.map((farmersmarket, index) => (
+            <Col key={index} xs={12} sm={8} md={5} lg={4}>
+              <Link
+              key={index}
+              to={`/farmersmarket/${encodeURIComponent(farmersmarket.title)}`} // Specify the URL for the farmers market
+              style={{ textDecoration: "none" }}
+              >
+                <FMCard
+                  title={farmersmarket.title}
+                  image={farmersmarket.image}
+                  location={farmersmarket.location}
+                  site={farmersmarket.site}
+                  hours={farmersmarket.hours}
+                  seasons={farmersmarket.seasons}
+                  vendors={farmersmarket.vendors}
+                />
+              </Link>
+            </Col>
+          ))}
+        </Container>
+      </Container>
+    </Container>
+  );
+};
+
+<div className="farmersmarkets">
       <h1 className="title" style={{ marginTop: "30px" }}>
         Farmers Markets
       </h1>
@@ -33,15 +66,16 @@ const FarmersMarkets = () => {
               <FMCard
                 title={farmersmarket.title}
                 image={farmersmarket.image}
-                description={farmersmarket.description}
-                nonprofits={farmersmarket.nonprofits}
+                location={farmersmarket.location}
+                site={farmersmarket.site}
+                hours={farmersmarket.hours}
+                seasons={farmersmarket.seasons}
+                vendors={farmersmarket.vendors}
               />
             </Link>
           ))}
         </div>
       </div>
     </div>
-  );
-};
 
-export default FarmersMarkets;
+export default FMGrid;
