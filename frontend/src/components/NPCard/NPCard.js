83f1942c7811677726599eb531958d04cac42609
Samarth Bhat <samarthbhat@utexas.edu>
2023-09-27 21:19:47 -0500

    added nonprofit cards - related to #18, #19

--- frontend/src/components/NPCard/NPCard.js
+++ frontend/src/components/NPCard/NPCard.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./NPCard.css";
+import Card from 'react-bootstrap/Card';
 
 // may have to import proptypes later?
 
-const NPCard = ({ title, image, description }) => {
+const NPCard = ({ title, image, county, NTEE_code, phone, employees }) => {
     return (
       // specifies CSS class used
-      <div className="np-card">
-        <img src={image} alt={title} className="np-card-image" />
-        <div className="np-card-details">
-          <h2 className="np-card-title">{title}</h2>
-          <p className="np-card-description">{description}</p>
-          {/* Add more details if needed */}
-        </div>
-      </div>
+      <Card className="np-card mb-4" style={{position: "relative" }}>
+        <Card.Body style={{position: "relative"}}>
+          <img src={image} alt={title} className="np-card-image mx-auto" />
+          <div className="np-card-details">
+            <h2 className="np-card-title" style={{ marginBottom: "10px" }}>{title}</h2>
+            <p className="np-card-county">{county}</p>
+            <p className="np-card-NTEE_code">{NTEE_code}</p>  
+            <p className="np-card-phone">{phone}</p>
+            <p className="np-card-employees">{employees}</p>
+          </div>
+        </Card.Body>
+      </Card>
     );
   };
   
--- frontend/src/pages/NonProfits.js
+++ frontend/src/pages/NonProfits.js
@@ -1,43 +1,49 @@
 import React from "react";
 import NPCard from "../components/NPCard/NPCard";
 import { Link } from "react-router-dom";
+import californiafresh from "../card-pics/nonprofits-pics/californiafresh.png";
+import cfe from "../card-pics/nonprofits-pics/cfe.jpg";
+import pfl from "../card-pics/nonprofits-pics/pfl.png";
+import { Container, Col } from "react-bootstrap/";
 
-const NonProfits = () => {
-  // Hard-coded data
-  const NPData = [
-    { title: "Nonprofit 1", image: "/placeholder.png", description: "Description 1" },
-    { title: "Nonprofit 2", image: "/placeholder.png", description: "Description 2" },
-    { title: "Nonprofit 3", image: "/placeholder.png", description: "Description 3" },
-  ];
+const NPData = [
+  { title: "California Fresh Farmers' Markets Association", image: californiafresh, county: "County: Fresno", NTEE_code: "NTEE Code: K03", phone: "Phone: (559) 417-7970", employees: "Num. Employees: 16"},
+  { title: "California Food Expo", image: cfe, county: "County: Fresno", NTEE_code: "NTEE Code: K31", phone: "Phone: (559) 227-9999", employees: "Num. Employees: 0" },
+  { title: "People Land and Food Foundation", image: pfl, county: "County: Fresno", NTEE_code: "NTEE Code: K20", phone: "Phone: (559) 855-3710", employees: "Num. Employees: 1" },
+];
 
+const NPGrid = () => {
   return (
-    <div className="nonprofits">
-      <h1 className="title" style={{ marginTop: "30px" }}>
-        Non-Profit Organizations
-      </h1>
-      {/* Title of page */}
-      <div className="centered-container">
-        {/* Centered container for cards */}
-        <div className="np-cards-container">
-          {/* Applies the container class in CSS file */}
-          {/* Render three NPCard components */}
-          {NPData.map((nonprofit, index) => (
-            <Link
-              key={index}
-              to={`/nonprofit/${encodeURIComponent(nonprofit.title)}`} // Specify the URL for the nonprofit
-              style={{ textDecoration: "none" }}
-            >
-              <NPCard
-                title={nonprofit.title}
-                image={nonprofit.image}
-                description={nonprofit.description}
-              />
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
+    <Container className="d-flex justify-content-center flex-column">
+      <Container className="container text-center mt-5 mb-4">
+        <h1>Non-Profit Organizations</h1>
+      </Container>
+
+      <Container className="px-4">
+        <Container className="row gx-3">
+          {( NPData.map((nonprofit, index) => (
+            <Col key={index} xs={12} sm={8} md={5} lg={4}>
+              <Link
+                key={index}
+                to={`/nonprofit/${encodeURIComponent(nonprofit.title)}`} // Specify the URL for the nonprofit
+                style={{ textDecoration: "none" }}
+              >
+                <NPCard
+                  title={nonprofit.title}
+                  image={nonprofit.image}
+                  county={nonprofit.county}
+                  NTEE_code={nonprofit.NTEE_code}
+                  phone={nonprofit.phone}
+                  employees={nonprofit.employees}
+                />
+              </Link>
+            </Col>
+            ))
+          )}
+        </Container>
+      </Container>
+    </Container>
   );
 };
 
-export default NonProfits;
+export default NPGrid;
