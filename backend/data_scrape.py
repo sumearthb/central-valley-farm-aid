5b7d1ad07d247a1532941b776af86c86faaa1ebe
Kunal Mody <kunalmody1@gmail.com>
2023-10-04 12:14:58 -0500

    SCRAPING SCRIPT ADDED

---
+++ backend/data_scrape.py
@@ -0,0 +1,60 @@
+import requests
+import mysql.connector
+
+# API endpoint to fetch data from
+api_url = "https://www.usdalocalfoodportal.com/fe/datasharing/"
+
+# MySQL database connection details
+db_config = {
+    "host": "YOUR_DB_HOST",
+    "user": "YOUR_DB_USER",
+    "password": "YOUR_DB_PASSWORD",
+    "database": "YOUR_DB_NAME",
+}
+
+# Function to fetch data from the API
+def fetch_data_from_api(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to fetch data from API. Status Code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Error fetching data from API: {str(e)}")
+        return None
+
+# Function to insert data into the MySQL database
+def insert_data_into_mysql(data, db_config):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        # Assuming 'data' is a list of dictionaries
+        for record in data:
+            # Customize this part to match your data structure
+            query = """
+                INSERT INTO your_table_name (column1, column2, ...)
+                VALUES (%s, %s, ...);
+            """
+            values = (record['column1'], record['column2'], ...)  # Replace with actual data
+            cursor.execute(query, values)
+
+        connection.commit()
+        print("Data successfully inserted into MySQL database.")
+
+    except mysql.connector.Error as err:
+        print(f"Error inserting data into MySQL: {err}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+if __name__ == "__main__":
+    # Fetch data from the API
+    api_data = fetch_data_from_api(api_url)
+
+    # if api_data:
+    #     # Insert data into the MySQL database
+    #     insert_data_into_mysql(api_data, db_config)
