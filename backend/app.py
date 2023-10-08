48af0e375d9d0b6b3449b4db76a3847d6ede7bb8
anthonydo8196 <anthonydo8196@gmail.com>
2023-10-07 21:07:47 -0500

    added some more files for setting up backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 # Import flask and datetime module for showing date and time
-from flask import Flask
+from flask import Flask, request, jsonify, Response
 import datetime
+from models import Animal, State, Park, app, db
+from schema import AnimalSchema, ParkSchema, StateSchema
+from flask_cors import CORS
+import json
+from sqlalchemy import or_, func, and_, Integer
+from sqlalchemy.sql.expression import asc, desc
+
 
 # python app.py to run dev server
 # Flask handles incoming HTTP req, uses SQLAlch to interact with database, then
@@ -17,17 +24,45 @@ app = Flask(__name__)
 def home():
 	return "Rural Farm Aid" 
 
-@app.route("/Locations")
-def getLocations():
-    return 
+def get_all_locations():
+    
+    return query
 
-@app.route("/NPs")
-def getNPs():
-    return 
+def get_all_NPs():
+     
+    return query
 
-@app.route("/FMs")
-def getFMs():
-    return 
+def get_all_FMs():
+     
+    return query
+
+@app.route("/api/Locations")
+def get_locations():
+    query = get_all_locations()
+    res = []
+    for location in query:
+
+    response = jsonify({"instance_count" : count})
+    return response
+
+@app.route("/api/NPs")
+def get_NPs():
+    query = get_all_NPs()
+    res = []
+    for location in query:
+
+    response = jsonify({"instance_count" : count})
+    return response 
+
+@app.route("/api/FMs")
+def get_FMs():
+    query = get_all_FMs()
+    res = []
+    for location in query:
+
+    response = jsonify({"instance_count" : count})
+    return response
+
 # Running app
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=5000, debug = True)
--- backend/models.py
+++ backend/models.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
+# Just need to change master password
 app = Flask(__name__)
 CORS(app)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://admin:020402020402@idb7-db.cyvindjupys7.us-east-2.rds.amazonaws.com:3306/idb7"
+] = "mysql+pymysql://admin:020402020402@api-scraping-database.crn5q2iybxzv.us-east-1.rds.amazonaws.com:3306/api-scraping-database"
 db = SQLAlchemy(app)
 
+# Add Association tables
+
 class Locations(db.Model):
+    __tablename__ = "locations"
     
 class FMs(db.Model):  
     __tablename__ = "something"
