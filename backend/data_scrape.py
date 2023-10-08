00d476a80f977bb354abc7550a121a71dfd8597d
akif.abidi1 <akif.abidi@utexas.edu>
2023-10-08 08:48:28 -0600

    nass.usda, i.e. location data, querying correctly

--- backend/data_scrape.py
+++ backend/data_scrape.py
@@ -16,25 +16,25 @@ db_config = {
     "database": "YOUR_DB_NAME",
 }
 
+location_data = {}
+
+
 # Function to fetch data from the API
 def fetch_location_crop_data():
-
     url = "https://quickstats.nass.usda.gov/api/api_GET/?key=2937E8A6-338E-3BD9-8E2E-1EF47FF8D729&sector_desc=crops&year=2018&state_alpha=CA&agg_level_desc=County&county_name=Glenn&county_name=Colusa&county_name=Kings&county_name=Kern&county_name=Sacremento&county_name=San Joaquin&county_name=Madera&county_name=Merced&county_name=Sutter&county_name=Yolo&county_name=Tulare&county_name=Tehama&county_name=Fresno"
-
     payload = "2937E8A6-338E-3BD9-8E2E-1EF47FF8D729\n"
     headers = {
     'Content-Type': 'text/plain',
     'Cookie': 'quickstats_session=8d2caf8817c780bfdf00f743ccb75759328623ad'
     }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-    try:
-        response = requests.get(url)
+    try: 
+        response = requests.request("GET", url, headers=headers, data=payload)
         if response.status_code == 200:
             return response.json()
         else:
-            print(f"Failed to fetch data from API. Status Code: {response.status_code}")
+            print(f"Failed to fetch data from nass.usda API. Status Code: {response.status_code}")
             return None
+    
     except Exception as excep:
         print(f"Error fetching data from API: {str(excep)}")
         return None
@@ -66,8 +66,9 @@ def insert_data_into_mysql(data, db_config):
             connection.close()
 
 if __name__ == "__main__":
-    for api_url in api_calls:
-        api_data = fetch_data_from_api(api_url)
-        if api_data:
-            # Insert data into the MySQL database
-            insert_data_into_mysql(api_data, db_config)
+    print(fetch_location_crop_data())
+    # for api_url in api_calls:
+    #     api_data = fetch_data_from_api(api_url)
+    #     if api_data:
+    #         # Insert data into the MySQL database
+    #         insert_data_into_mysql(api_data, db_config)
