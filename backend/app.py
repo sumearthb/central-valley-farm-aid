398f69c9d30884291904648b4da20c45ea4790f9
Samarth Bhat <samarthbhat@utexas.edu>
2023-10-09 02:44:55 -0500

    added some basic functionality for API endpoints - related to #52

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 # Import flask and datetime module for showing date and time
 from flask import Flask, request, jsonify, Response
 import datetime
-from models import Animal, State, Park, app, db
-from schema import AnimalSchema, ParkSchema, StateSchema
+from models import Locations, NPs, FMs, app, db
+from schema import LocationSchema, NPSchema, FMSchema
 from flask_cors import CORS
 import json
 from sqlalchemy import or_, func, and_, Integer
@@ -13,54 +13,57 @@ from sqlalchemy.sql.expression import asc, desc
 # Flask handles incoming HTTP req, uses SQLAlch to interact with database, then
 # marshmallow schemas to format data before sending it as json response to clients
 
-x = datetime.datetime.now()
-
 # Initializing flask app
 app = Flask(__name__)
 
+# size for each page -- pagination
+PAGE_SIZE = 20
 
 # Route for seeing a data
 @app.route('/')
 def home():
-	return "Rural Farm Aid" 
+	return "Welcome to Central Valley Farm Aid!" 
 
 def get_all_locations():
-    
+    query = db.session.query(Locations)
     return query
 
 def get_all_NPs():
-     
+    query = db.session.query(NPs)
     return query
 
 def get_all_FMs():
-     
+    query = db.session.query(FMs)
     return query
 
 @app.route("/api/Locations")
 def get_locations():
     query = get_all_locations()
-    res = []
+    location_list = []
     for location in query:
-
-    response = jsonify({"instance_count" : count})
+        location_dict = {}
+        location_list.append(location_dict)
+    response = jsonify({"instance_count" : len(location_list), "locations" : location_list})
     return response
 
 @app.route("/api/NPs")
 def get_NPs():
     query = get_all_NPs()
-    res = []
-    for location in query:
-
-    response = jsonify({"instance_count" : count})
+    NP_list = []
+    for NP in query:
+        NP_dict = {}
+        NP_list.append(NP_dict)
+    response = jsonify({"instance_count" : len(NP_list), "locations" : NP_list})
     return response 
 
 @app.route("/api/FMs")
 def get_FMs():
     query = get_all_FMs()
-    res = []
-    for location in query:
-
-    response = jsonify({"instance_count" : count})
+    FM_list = []
+    for FM in query:
+        FM_dict = {}
+        FM_list.append(FM_dict)
+    response = jsonify({"instance_count" : len(FM_list), "locations" : FM_list})
     return response
 
 # Running app
