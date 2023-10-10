a07379d4faaab4510311d34e5fae6e623c3bd698
akif.abidi1 <akif.abidi@utexas.edu>
2023-10-09 23:31:07 -0500

    small updates

--- backend/data_scrape.py
+++ backend/data_scrape.py
@@ -18,25 +18,70 @@ db_config = {
     "database": "YOUR_DB_NAME",
 }
 
-
+#------------------------------------------------------------------------
+# CHARITY DATA
 def fetch_charity_data() -> dict:
     # We have two API calls here. The API uses search terms to get relevant queries and cannot 
     # query more than one search term at once, so we do it seperetly
+    res1 = {}
+    res2 = {}
 
     # Query one: search term "farmer market"
-    url = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmer market&state=CA&rows=100"
+    url1 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmer market&state=CA&rows=100"
     payload = {}
     headers = {
         'Cookie': 'laravel_session=eyJpdiI6IlBvbHJLTDR0SFh3SWdaOXJRbkRzXC9EQk9tUjlRMnVXWmFIMGdmbzgzWU9jPSIsInZhbHVlIjoic204QVBDdFpPSkRKYmZ4RUlxNW1oc3Y2eittR2ZscHR1S01ya2FuYjFjWnFpeUxTeE44SEdxTlhneE82ZWFlMjJsbFdqdGhoWUdXbzBkb3FwcHRlc0E9PSIsIm1hYyI6IjM2ZGZjYmRhOTE4ZTM4MjIwMDUwMmZmY2Q5OTE0NjdjOGE4OThjMTU0YjY5ZjE5ZmJlZmI5MzZjODljYzMxODMifQ%3D%3D'
     }
 
+
     try:
-        response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request("GET", url1, headers=headers, data=payload)
         if response.status_code == 200:
             res1: dict = response.json()
-            print("CHARITY DATA-----", res1.data.count)
+            print("CHARITY DATA 1-----", res1)
         else:
-            print(f"Failed to fetch data from charity API. Status Code: {response.status_code}")
+            print(f"Failed to fetch data from charity API 1. Status Code: {response.status_code}")
+            return None
+
+    except Exception as excep:
+        print(f"Error fetching data from API: {str(excep)}")
+        return None
+
+
+    # Query two: search term "farmers market"
+    url2 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmers market&state=CA"
+    payload = {}
+    headers = {
+    'Cookie': 'laravel_session=eyJpdiI6InhLMkg4TENYaGpXR0JWT0I1aXA1amR3SkVGWERJVG1KcUZDaWZJc1I4dXc9IiwidmFsdWUiOiJpTTl4bVlqZStTc0pESjh1VjR0YzVzZE5NMURPZDNYcXgxWFFkclQrV1JFOG5tXC9CNXdmRmUrT2grSU1jRXIzRmo5S2Q3UnZPdzFCMHFCMlM3SXp2QVE9PSIsIm1hYyI6ImRkNjZjYTdmMGM0MTI2MjZjMjg3MGZiMmY0YzU5MTRjYjM3OTI5Mjk4Nzk2ZWQ0YjY2MmYwNTRlZWViZmExMDcifQ%3D%3D'
+    }
+
+    try:
+        response = requests.request("GET", url2, headers=headers, data=payload)
+        if response.status_code == 200:
+            res2: dict = response.json()
+            print("CHARITY DATA 2-----", res2)
+        else:
+            print(f"Failed to fetch data from charity API 2. Status Code: {response.status_code}")
+            return None
+
+    except Exception as excep:
+        print(f"Error fetching data from API: {str(excep)}")
+        return None
+
+    # Query three: search term "farmer" with Food Category to narrow down
+    url3 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmer&state=CA&rows=100&category=K"
+    payload = {}
+    headers = {
+    'Cookie': 'laravel_session=eyJpdiI6IkhLWTNDWFwveXVuM3diK1k4aThFK1RNcFpyVVIrd3hKanRxU3Q5eG52XC9xcz0iLCJ2YWx1ZSI6IisxSkoxU1pkRzJqeXh5UkNoZkJkU084RUpUc2RienZXNTNjN3V3QVFYaXB3QWo5T0g4ZDZhRG5vQ2hwWkZlMVR6ZHdqQVZQRm44dHZIMTBFVWx6UHlRPT0iLCJtYWMiOiIzZDUzNWM4MjgwMGMzODY1OGU0NTUwYjRmOTIzN2UwZTgxMTI4MzU0ZDVmOGIwYTkyNzU5ODNjYWFjMDBmOWMyIn0%3D'
+    }
+    
+    try:
+        response = requests.request("GET", url3, headers=headers, data=payload)
+        if response.status_code == 200:
+            res3: dict = response.json()
+            print("CHARITY DATA 3-----", res3)
+        else:
+            print(f"Failed to fetch data from charity API 4. Status Code: {response.status_code}")
             return None
 
     except Exception as excep:
@@ -44,13 +89,15 @@ def fetch_charity_data() -> dict:
         return None
 
     
+    # Join the seperate API calls
     res: dict = res1.update(res2)
+    res = res.update(res3)
 
     return res
 
-
-# Function to fetch data from the API
-def fetch_location_crop_data():
+#------------------------------------------------------------------------
+# CROP DATA
+def fetch_location_crop_data() -> dict:
     url = "https://quickstats.nass.usda.gov/api/api_GET/?key=2937E8A6-338E-3BD9-8E2E-1EF47FF8D729&sector_desc=crops&year=2018&state_alpha=CA&agg_level_desc=County&county_name=Glenn&county_name=Colusa&county_name=Kings&county_name=Kern&county_name=Sacremento&county_name=San Joaquin&county_name=Madera&county_name=Merced&county_name=Sutter&county_name=Yolo&county_name=Tulare&county_name=Tehama&county_name=Fresno&county_name=Stanislaus&county_name=Butte"
     payload = "2937E8A6-338E-3BD9-8E2E-1EF47FF8D729\n"
     headers = {
