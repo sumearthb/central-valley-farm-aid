842374adb1d86d7a8896b5a4c20f4b04af81e953
akif.abidi1 <akif.abidi@utexas.edu>
2023-10-11 12:30:42 -0500

    A lot of SQL query changes - crop and location data set up

--- backend/data_scrape.py
+++ backend/data_scrape.py
@@ -90,7 +90,6 @@ def create_charity_table():
         statusCd INT,
         website VARCHAR(255),
         missionStatement TEXT,
-        parent_ein VARCHAR(20),
         latitude DECIMAL(10, 6),
         longitude DECIMAL(10, 6)
     );
@@ -115,100 +114,7 @@ def create_charity_table():
         # Close the cursor and connection
         cursor.close()
         connection.close()
-#------------------------------------------------------------------------
-# CHARITY DATA
-def fetch_charity_data() -> dict:
-    # We have two API calls here. The API uses search terms to get relevant queries and cannot 
-    # query more than one search term at once, so we do it seperetly
-    res1 = {}
-    res2 = {}
 
-    # Query one: search term "farmer market"
-    url1 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmer market&state=CA&rows=100"
-    payload = {}
-    headers = {
-        'Cookie': 'laravel_session=eyJpdiI6IlBvbHJLTDR0SFh3SWdaOXJRbkRzXC9EQk9tUjlRMnVXWmFIMGdmbzgzWU9jPSIsInZhbHVlIjoic204QVBDdFpPSkRKYmZ4RUlxNW1oc3Y2eittR2ZscHR1S01ya2FuYjFjWnFpeUxTeE44SEdxTlhneE82ZWFlMjJsbFdqdGhoWUdXbzBkb3FwcHRlc0E9PSIsIm1hYyI6IjM2ZGZjYmRhOTE4ZTM4MjIwMDUwMmZmY2Q5OTE0NjdjOGE4OThjMTU0YjY5ZjE5ZmJlZmI5MzZjODljYzMxODMifQ%3D%3D'
-    }
-
-
-    try:
-        response = requests.request("GET", url1, headers=headers, data=payload)
-        if response.status_code == 200:
-            res1: dict = response.json()
-            print("CHARITY DATA 1-----", res1)
-        else:
-            print(f"Failed to fetch data from charity API 1. Status Code: {response.status_code}")
-            return None
-
-    except Exception as excep:
-        print(f"Error fetching data from API: {str(excep)}")
-        return None
-
-
-    # Query two: search term "farmers market"
-    url2 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmers market&state=CA"
-    payload = {}
-    headers = {
-    'Cookie': 'laravel_session=eyJpdiI6InhLMkg4TENYaGpXR0JWT0I1aXA1amR3SkVGWERJVG1KcUZDaWZJc1I4dXc9IiwidmFsdWUiOiJpTTl4bVlqZStTc0pESjh1VjR0YzVzZE5NMURPZDNYcXgxWFFkclQrV1JFOG5tXC9CNXdmRmUrT2grSU1jRXIzRmo5S2Q3UnZPdzFCMHFCMlM3SXp2QVE9PSIsIm1hYyI6ImRkNjZjYTdmMGM0MTI2MjZjMjg3MGZiMmY0YzU5MTRjYjM3OTI5Mjk4Nzk2ZWQ0YjY2MmYwNTRlZWViZmExMDcifQ%3D%3D'
-    }
-
-    try:
-        response = requests.request("GET", url2, headers=headers, data=payload)
-        if response.status_code == 200:
-            res2: dict = response.json()
-            print("CHARITY DATA 2-----", res2)
-        else:
-            print(f"Failed to fetch data from charity API 2. Status Code: {response.status_code}")
-            return None
-
-    except Exception as excep:
-        print(f"Error fetching data from API: {str(excep)}")
-        return None
-
-    # Query three: search term "farmer" with Food Category to narrow down
-    url3 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmer&state=CA&rows=100&category=K"
-    payload = {}
-    headers = {
-    'Cookie': 'laravel_session=eyJpdiI6IkhLWTNDWFwveXVuM3diK1k4aThFK1RNcFpyVVIrd3hKanRxU3Q5eG52XC9xcz0iLCJ2YWx1ZSI6IisxSkoxU1pkRzJqeXh5UkNoZkJkU084RUpUc2RienZXNTNjN3V3QVFYaXB3QWo5T0g4ZDZhRG5vQ2hwWkZlMVR6ZHdqQVZQRm44dHZIMTBFVWx6UHlRPT0iLCJtYWMiOiIzZDUzNWM4MjgwMGMzODY1OGU0NTUwYjRmOTIzN2UwZTgxMTI4MzU0ZDVmOGIwYTkyNzU5ODNjYWFjMDBmOWMyIn0%3D'
-    }
-    
-    try:
-        response = requests.request("GET", url3, headers=headers, data=payload)
-        if response.status_code == 200:
-            res3: dict = response.json()
-            print("CHARITY DATA 3-----", res3)
-        else:
-            print(f"Failed to fetch data from charity API 4. Status Code: {response.status_code}")
-            return None
-
-    except Exception as excep:
-        print(f"Error fetching data from API: {str(excep)}")
-        return None
-
-    
-    # Join the seperate API calls
-    res: dict = res1.update(res2)
-    res = res.update(res3)
-
-    return res
-
-def insert_charity_crop_data_to_db(db_config):
-    data = fetch_charity_data()
-
-    try:
-        connection = mysql.connector.connect(**db_config)
-        cursor = connection.cursor()
-        
-        for record in data:
-            # TODO
-            pass
-             
-    except mysql.connector.Error as err:
-        print(f"Error inserting data into MySQL: {err}")
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
 #------------------------------------------------------------------------
 # CROP DATA
 def fetch_location_crop_data() -> dict:
@@ -223,8 +129,7 @@ def fetch_location_crop_data() -> dict:
         response = requests.request("GET", url, headers=headers, data=payload)
         if response.status_code == 200:
             res = response.json()
-            print("CROP DATA-----", res.data.count)
-            return response.json()
+            return res['data']
         else:
             print(f"Failed to fetch data from nass.usda API. Status Code: {response.status_code}")
             return None
@@ -290,8 +195,8 @@ def insert_location_crop_data_to_db(db_config):
                     %(asd_desc)s
                 )
             '''
-            values = (record['column1'], record['column2'], ...)  # Replace with actual data
-            cursor.execute(insert_query, values)
+
+            cursor.execute(insert_query, record)
 
         connection.commit()
         print("Location_crop successfully inserted into MySQL database.")
@@ -303,12 +208,135 @@ def insert_location_crop_data_to_db(db_config):
             cursor.close()
             connection.close()
 
-if __name__ == "__main__":
+#------------------------------------------------------------------------
+# CHARITY DATA
+def fetch_charity_data() -> dict:
+    # We have two API calls here. The API uses search terms to get relevant queries and cannot 
+    # query more than one search term at once, so we do it seperetly
+    res1 = {}
+    res2 = {}
 
+    # Query one: search term "farmer market"
+    url1 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmer market&state=CA&rows=100"
+    payload = {}
+    headers = {
+        'Cookie': 'laravel_session=eyJpdiI6IlBvbHJLTDR0SFh3SWdaOXJRbkRzXC9EQk9tUjlRMnVXWmFIMGdmbzgzWU9jPSIsInZhbHVlIjoic204QVBDdFpPSkRKYmZ4RUlxNW1oc3Y2eittR2ZscHR1S01ya2FuYjFjWnFpeUxTeE44SEdxTlhneE82ZWFlMjJsbFdqdGhoWUdXbzBkb3FwcHRlc0E9PSIsIm1hYyI6IjM2ZGZjYmRhOTE4ZTM4MjIwMDUwMmZmY2Q5OTE0NjdjOGE4OThjMTU0YjY5ZjE5ZmJlZmI5MzZjODljYzMxODMifQ%3D%3D'
+    }
+
+
+    try:
+        response = requests.request("GET", url1, headers=headers, data=payload)
+        if response.status_code == 200:
+            res1: dict = response.json()
+        else:
+            print(f"Failed to fetch data from charity API 1. Status Code: {response.status_code}")
+            return None
+
+    except Exception as excep:
+        print(f"Error fetching data from API: {str(excep)}")
+        return None
+
+
+    # Query two: search term "farmers market"
+    url2 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmers market&state=CA"
+    payload = {}
+    headers = {
+    'Cookie': 'laravel_session=eyJpdiI6InhLMkg4TENYaGpXR0JWT0I1aXA1amR3SkVGWERJVG1KcUZDaWZJc1I4dXc9IiwidmFsdWUiOiJpTTl4bVlqZStTc0pESjh1VjR0YzVzZE5NMURPZDNYcXgxWFFkclQrV1JFOG5tXC9CNXdmRmUrT2grSU1jRXIzRmo5S2Q3UnZPdzFCMHFCMlM3SXp2QVE9PSIsIm1hYyI6ImRkNjZjYTdmMGM0MTI2MjZjMjg3MGZiMmY0YzU5MTRjYjM3OTI5Mjk4Nzk2ZWQ0YjY2MmYwNTRlZWViZmExMDcifQ%3D%3D'
+    }
+
+    try:
+        response = requests.request("GET", url2, headers=headers, data=payload)
+        if response.status_code == 200:
+            res2: dict = response.json()
+            #print("CHARITY DATA 2-----", res2)
+        else:
+            print(f"Failed to fetch data from charity API 2. Status Code: {response.status_code}")
+            return None
+
+    except Exception as excep:
+        print(f"Error fetching data from API: {str(excep)}")
+        return None
+
+    # Query three: search term "farmer" with Food Category to narrow down
+    url3 = "http://data.orghunter.com/v1/charitysearch?user_key=6abaefdede85bfc1b540f4e9d6ff7882&searchTerm=farmer&state=CA&rows=100&category=K"
+    payload = {}
+    headers = {
+    'Cookie': 'laravel_session=eyJpdiI6IkhLWTNDWFwveXVuM3diK1k4aThFK1RNcFpyVVIrd3hKanRxU3Q5eG52XC9xcz0iLCJ2YWx1ZSI6IisxSkoxU1pkRzJqeXh5UkNoZkJkU084RUpUc2RienZXNTNjN3V3QVFYaXB3QWo5T0g4ZDZhRG5vQ2hwWkZlMVR6ZHdqQVZQRm44dHZIMTBFVWx6UHlRPT0iLCJtYWMiOiIzZDUzNWM4MjgwMGMzODY1OGU0NTUwYjRmOTIzN2UwZTgxMTI4MzU0ZDVmOGIwYTkyNzU5ODNjYWFjMDBmOWMyIn0%3D'
+    }
+    
+    try:
+        response = requests.request("GET", url3, headers=headers, data=payload)
+        if response.status_code == 200:
+            res3: dict = response.json()
+            #print("CHARITY DATA 3-----", res3)
+        else:
+            print(f"Failed to fetch data from charity API 4. Status Code: {response.status_code}")
+            return None
+
+    except Exception as excep:
+        print(f"Error fetching data from API: {str(excep)}")
+        return None
+
+    
+    # Join the seperate API calls in a singular list of data points
+    res: list = res1['data']
+    res.append(res2['data'])
+    res.append(res3['data'])
+    print(res)
+    return res
+
+def insert_charity_crop_data_to_db(db_config):
+    data = fetch_charity_data()
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+         # Define the SQL INSERT statement
+        insert_query = """
+        INSERT INTO your_table_name (
+            ein, charityName, url, donationUrl, city, state, zipCode,
+            start, rows, recordCount, score, acceptingDonations,
+            category, eligibleCd, deductibilityCd, statusCd, website,
+            missionStatement, latitude, longitude
+        ) VALUES (
+            %(ein)s, %(charityName)s, %(url)s, %(donationUrl)s, %(city)s, %(state)s, %(zipCode)s,
+            %(start)s, %(rows)s, %(recordCount)s, %(score)s, %(acceptingDonations)s,
+            %(category)s, %(eligibleCd)s, %(deductibilityCd)s, %(statusCd)s, %(website)s,
+            %(missionStatement)s, %(latitude)s, %(longitude)s
+        """
+        
+        for record in data:
+            # Iterate through the API data and insert it into the MySQL database
+            cursor.execute(insert_query, data)
+
+        # Commit the changes to the database
+        connection.commit()
+
+        print("Data inserted into the database successfully.")
+
+    except mysql.connector.Error as err:
+        print("Error inserting data into the database:", err)
+
+    finally:
+        # Close the cursor and connection
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+
+# Defining main function
+def main(): 
     # location crop data
     create_crop_table() # does not create new table if it exists
     insert_location_crop_data_to_db(db_config=db_config)
 
     # charities data
-    create_charity_table
+    create_charity_table()
+    fetch_charity_data(db_config=db_config)
+
+  
+
+if __name__=="__main__": 
+    main() 
+
 
