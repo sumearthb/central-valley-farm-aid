85dedabd2e80ca3dc408104a06aa238df6b915cd
Samarth Bhat <samarthbhat@utexas.edu>
2023-10-16 01:07:43 -0500

    added new instructions to pipeline .yml; added more unit tests; added more instructions to Makefile - related to #55, #67

--- Makefile
+++ Makefile
@@ -4,19 +4,45 @@ SHELL := bash
 clean: 
 	rm -f *.tmp
 
-run: 
-	echo "Running application..."
-	cd ./frontend/ && npm start
-	echo "Successfully ran"
-
 pull:
 	make clean
 	@echo
 	git pull
 	git status
 
+status:
+	make clean
+	@echo
+	git branch
+	git remote -v
+	git status
+
+log:
+	git log > idb.log.txt
+
+run-frontend-local: 
+	echo "Running application..."
+	cd ./frontend/ && npm start
+	echo "Successfully ran"
+
+# test-frontend:
+# 	echo "Running frontend unit tests..."
+# 	cd ./frontend/ && ...
+# 	echo "Successfully ran tests"
+
+test-backend:
+	echo "Running backend unit tests..."
+	cd ./backend/ && python3 tests.py
+	echo "Successfully ran tests"
+
 build-frontend :
 	docker build -t idb-frontend frontend/
 
 run-frontend:
 	docker run -dp 80:3000 idb-frontend
+
+# build-backend:
+# 	docker build ...
+
+# run-backend:
+# 	docker ...
--- backend/tests.py
+++ backend/tests.py
@@ -15,33 +15,58 @@ class Tests(unittest.TestCase):
 
     def test_get_all_locations(self):
         with self.client:
-            response = self.client.get("/api/getAllLocations")
+            response = self.client.get("/api/GetAllLocations")
             self.assertEqual(response.status_code, 200)
             data = response.json["data"]
+            self.assertIsNotNone(data)
             # TODO ~ change assert value to actual expected instance count
             self.assertEqual(response.json["instance_count"], len(data))
 
-    def test_get_location(self):
-        pass
+    def test_get_location_by_id(self):
+        response = self.app.get("/api/GetLocation/x")
+        self.assertEqual(response.status_code, 200)
+        data = response.json
+        self.assertIsNotNone(data)
+        self.assertEqual(data["id"], "x")
+
+    def test_get_NP(self):
+        pass   
 
     def test_get_all_NPs(self):
         with self.client:
-            response = self.client.get("/api/getAllLocations")
+            response = self.client.get("/api/GetAllNonprofit")
             self.assertEqual(response.status_code, 200)
             data = response.json["data"]
+            self.assertIsNotNone(data)
             # TODO ~ change assert value to actual expected instance count
             self.assertEqual(response.json["instance_count"], len(data))
     
-    def test_get_location(self):
+    def test_get_NP_by_id(self):
+        response = self.app.get("/api/GetNonProfit/x")
+        self.assertEqual(response.status_code, 200)
+        data = response.json
+        self.assertIsNotNone(data)
+        self.assertEqual(data["id"], "x")
+        
+    def test_get_FM(self):
         pass
     
     def test_get_all_FMs(self):
         with self.client:
-            response = self.client.get("/api/getAllLocations")
+            response = self.client.get("/api/GetAllMarkets")
             self.assertEqual(response.status_code, 200)
             data = response.json["data"]
+            self.assertIsNotNone(data)
             # TODO ~ change assert value to actual expected instance count
             self.assertEqual(response.json["instance_count"], len(data))
+
+    def test_get_FM_by_id(self):
+        response = self.app.get("/api/GetMarket/x")
+        self.assertEqual(response.status_code, 200)
+        data = response.json
+        self.assertIsNotNone(data)
+        self.assertEqual(data["id"], "x")
+
 
 if __name__ == '__main__':
     unittest.main()    
