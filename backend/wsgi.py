6d71d9181bd93656180786886154825d6b88e6bb
Samarth Bhat <samarthbhat@utexas.edu>
2023-10-19 14:19:38 -0500

    trying wsgi - related to #11

---
+++ backend/wsgi.py
@@ -0,0 +1,10 @@
+# from waitress import serve
+# from app import app
+
+# if __name__ == "__main__":
+#     serve(app, host='0.0.0.0', port=80)
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()
