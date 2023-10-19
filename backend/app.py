3a0e866c70ec20fe9947de9f768dbc76434048d3
samarthbhat <samarthbhat@utexas.edu>
2023-10-19 14:45:46 -0500

    fixing Dockerfile - related to #11

--- backend/app.py
+++ backend/app.py
@@ -123,5 +123,5 @@ def paginate(query, page_num, page_size=PAGE_SIZE):
 
 # Running app
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000, debug = True)
+	app.run(host="0.0.0.0", port=8080, debug = True)
 
