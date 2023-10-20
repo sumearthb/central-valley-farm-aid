3a414e60020628def1c88dbefaf1a23a3a3873df
Samarth Bhat <samarthbhat@utexas.edu>
2023-10-20 17:39:06 -0500

    grabbed images from Google Maps API - related to #52

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,10 @@ from schema import LocationSchema, NPSchema, FMSchema
 from flask_cors import CORS
 from sqlalchemy import or_, func, and_, Integer
 from sqlalchemy.sql.expression import asc, desc
+from countydata import get_all_counties_in_california
+import googlemaps
+
+gmaps = googlemaps.Client(key='AIzaSyBMJJbFxLfnX8DpE_BGF2dF8t5aWSQJOOs')
 
 # size for each page -- pagination
 PER_PAGE = 9
@@ -42,9 +46,31 @@ def get_all_locations():
     if page is not None:
         query = paginate(query, int(page), int(per_page))
     location_list = []
+    int
     for location in query:
         location_schema = LocationSchema()
         location_dict = location_schema.dump(location)
+
+        # Default Value
+        photo_references = []
+
+        # Call Google API
+        find_place = gmaps.find_place(
+            location_dict["location"] + " County, CA",
+            "textquery",
+            fields=["photos"]) # This is the different fields u can get from a place, for locations we only want photos
+        
+        # Add data if exists
+        if (len(find_place["candidates"]) > 0):
+            print("", find_place["candidates"])
+            photo_references = []
+            photos = find_place["candidates"][0].get("photos", [])
+            for photo in photos:
+                photo_references.append(photo["photo_reference"])
+        location_dict["photo_references"] = photo_references
+
+        #location_dict["county_scraped"] = get_all_counties_in_california()
+
         location_list.append(location_dict)
     response = jsonify({"instance_count" : len(location_list), "data" : location_list})
     return response
@@ -71,6 +97,31 @@ def get_all_nonprofits():
     for NP in query:
         NP_schema = NPSchema()
         NP_dict = NP_schema.dump(NP)
+        # find_place = gmaps.find_place(
+        #     NP_dict["charityName"],
+        #     "textquery",
+        #     fields=["photos"]) # This is the different fields u can get from a place, for locations we only want photos
+
+        # photo_reference = find_place['candidates'][0]['photos'][0]['photo_reference']
+        # NP_dict["photo_reference"] = photo_reference
+        # Default Value
+        photo_references = []
+        bias_string = "circle:1000000@{},{}".format(NP_dict.get("latitude", 37.2917), NP_dict.get("longitude", -119.9789))
+        # Call Google API
+        find_place = gmaps.find_place(
+            NP_dict["charityName"] + " nonprofit",
+            "textquery",
+            location_bias = bias_string,
+            fields=["photos"]) # This is the different fields u can get from a place, for locations we only want photos
+        
+        # Add data if exists
+        if (len(find_place["candidates"]) > 0):
+            print("", find_place["candidates"])
+            photo_references = []
+            photos = find_place["candidates"][0].get("photos", [])
+            for photo in photos:
+                photo_references.append(photo["photo_reference"])
+        NP_dict["photo_references"] = photo_references
         NP_list.append(NP_dict)
     response = jsonify({"instance_count" : len(NP_list), "data" : NP_list})
     return response 
@@ -97,6 +148,43 @@ def get_all_markets():
     for FM in query:
         FM_schema = FMSchema()
         FM_dict = FM_schema.dump(FM)
+
+        # Default Values
+        website = ""
+        phone = ""
+        rating = 0
+        wheelchair_accessible = False
+        photo_references = []
+
+        # Call Google API
+        bias_string = "circle:3000@{},{}".format(FM_dict["location_y"], FM_dict["location_x"])
+        find_place = gmaps.find_place(
+            FM_dict["listing_name"],
+            "textquery",
+            location_bias=bias_string,
+            fields=["place_id"]) # Get the place_id because google API is stupid
+        places: list = find_place['candidates']
+
+        # Add data if exists
+        if (len(places) > 0):
+            place_id = places[0]["place_id"]
+            
+            place_dict: dict = gmaps.place(place_id)["result"]
+
+            website = place_dict.get("website", "")
+            phone = place_dict.get("formatted_phone_number", "")
+            rating = place_dict.get("rating")
+            wheelchair_accessible = place_dict.get("wheelchair_accessible_entrance", False)
+            photos = place_dict.get("photos", [])
+            for photo in photos:
+                photo_references.append(photo["photo_reference"])
+
+            FM_dict["website"] = website
+            FM_dict["phone"] = phone
+            FM_dict["rating"] = rating
+            FM_dict["wheelchair_accessible"] = wheelchair_accessible
+            FM_dict["photo_references"] = photo_references
+        
         FM_list.append(FM_dict)
     response = jsonify({"instance_count" : len(FM_list), "data" : FM_list})
     return response
