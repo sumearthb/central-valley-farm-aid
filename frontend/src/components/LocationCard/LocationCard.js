e1109ce31ed3a5b451b942a8486ccefbc158fe82
Martin Murtiono <martin.murtiono@gmail.com>
2023-10-20 09:00:23 -0500

    stuff

--- frontend/src/components/LocationCard/LocationCard.js
+++ frontend/src/components/LocationCard/LocationCard.js
@@ -8,11 +8,15 @@ import { Link } from 'react-router-dom';
 const LocationCard = ({ title, crops }) => {
     return (
       // specifies CSS class used
-      <Card className="location-card d-flex flex-column">
-        {/* <img src={image} alt={title} className="location-card-image" /> */}
-        <div className="location-card-details">
-          <h2 className="location-card-title">{title}</h2>
-          <p className="location-card-crop">{crops}</p>
+      <Card className="location-card">
+        <Card.Img src={`https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBMJJbFxLfnX8DpE_BGF2dF8t5aWSQJOOs&center=${title} County, CA&zoom=8&size=300x400`} alt={title} className="location-card-image" />
+        <Card.Body className="location-card-body">
+          <Card.Title className="location-card-title">
+            {title}
+          </Card.Title>
+          <Card.Text className="location-card-crop">
+            # of Crops: {crops.length}
+          </Card.Text>
           <Link
             to={`/location/${title}`}
             style={{ textDecoration: "none" }}
@@ -20,7 +24,7 @@ const LocationCard = ({ title, crops }) => {
             <Button variant="primary" style={{ backgroundColor: '#69A765'}} className="align-self-end border-white">More info</Button>
           </Link>
           {/* Add more details if needed */}
-        </div>
+        </Card.Body>
       </Card>
     );
   };
