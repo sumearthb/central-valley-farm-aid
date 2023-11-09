14e1fdb44529b0bd5398bdbd94eb277718062d6b
Martin Murtiono <martin.murtiono@gmail.com>
2023-11-08 22:23:55 -0600

    fix again...

--- frontend/acceptance_tests/runTests.py
+++ frontend/acceptance_tests/runTests.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
         print("Unsupported OS")
         exit(-1)
 
-    os.system("python3 ./navBarTests.py")
+    os.system("python3 ./acceptance_tests/navBarTests.py")
 
