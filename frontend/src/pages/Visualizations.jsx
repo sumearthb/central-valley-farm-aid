e6657f4352c7614e9f38f4e40b05f3394e952fc4
anthonydo8196 <anthonydo8196@gmail.com>
2023-11-29 16:55:58 -0600

    fixed resizing problem, related to #104

--- frontend/src/pages/Visualizations.jsx
+++ frontend/src/pages/Visualizations.jsx
@@ -18,59 +18,76 @@ const Visualizations = () => {
   }, []);
 
   useEffect(() => {
+    const handleResize = () => {
+      renderChart();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [locations]);
+
+  const renderChart = () => {
     // Checks if data is available before rendering chart
     if (locations.length > 0) {
-      const data = locations.map((location) => ({
-        name: location.name,
-        numCrops: location.crops.crops.length,
-      }));
+      // If we want to order by the largest number of crops
+      // locations.sort((a, b) => b.crops.crops.length - a.crops.crops.length);
 
       // Chart dimensions
       const width = 1200;
       const height = width / 2;
-      const margin = { top: 50, right: 50, bottom: 70, left: 70 };
+      const margin = { top: 50, right: 50, bottom: 90, left: 70 };
 
-      // If we want to order by largest number of crops
-      //data.sort((a, b) => b.numCrops - a.numCrops);
+      // Makes plot dynamic to page size
+      const containerWidth = visualizationRef.current.clientWidth;
+      const containerHeight = height;
+
+      const updatedWidth = containerWidth - margin.left - margin.right;
+      const updatedHeight = containerHeight - margin.top - margin.bottom;
 
       // Scales
       const xScale = d3
         .scaleBand()
-        .domain(data.map((d) => d.name))
-        .range([margin.left, width - margin.right])
+        .domain(locations.map((location) => location.name))
+        .range([margin.left, updatedWidth - margin.right])
         .padding(0.2);
 
       const yScale = d3
         .scaleLinear()
-        .domain([0, d3.max(data, (d) => d.numCrops)])
-        .range([height - margin.bottom, margin.top]);
+        .domain([0, d3.max(locations, (location) => location.crops.crops.length)])
+        .range([updatedHeight - margin.bottom, margin.top]);
 
-      // Clears previous chart for data change but data won't change
-      //d3.select(visualizationRef.current).selectAll("*").remove();
+      // Clear previous chart
+      d3.select(visualizationRef.current).selectAll("*").remove();
 
       // Container for bar plot 
-      const svg = d3.select(visualizationRef.current).append("svg").attr("width", width).attr("height", height);
+      const svg = d3.select(visualizationRef.current).append("svg").attr("width", updatedWidth).attr("height", updatedHeight);
 
       // Bars data
       svg
         .selectAll("rect")
-        .data(data)
+        .data(locations)
         .enter()
         .append("rect")
-        .attr("x", (d) => xScale(d.name))
-        .attr("y", (d) => yScale(d.numCrops))
+        .attr("x", (location) => xScale(location.name))
+        .attr("y", (location) => yScale(location.crops.crops.length))
         .attr("width", xScale.bandwidth())
-        .attr("height", (d) => height - margin.bottom - yScale(d.numCrops))
+        .attr("height", (location) => updatedHeight - margin.bottom - yScale(location.crops.crops.length))
         .attr("fill", "steelblue");
 
       // X-axis
       svg
-        .append("g")
-        .attr("transform", `translate(0, ${height - margin.bottom})`)
-        .call(d3.axisBottom(xScale).tickSizeOuter(0))
-        .selectAll("text")
-        .attr("transform", "rotate(-45)")
-        .style("text-anchor", "end");
+      .append("g")
+      .attr("transform", `translate(0, ${updatedHeight - margin.bottom})`)
+      .call(d3.axisBottom(xScale).tickSizeOuter(0))
+      .selectAll("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -10)
+      .attr("y", -4)
+      .style("text-anchor", "end");
 
       // Y-axis
       svg.append("g").attr("transform", `translate(${margin.left}, 0)`).call(d3.axisLeft(yScale));
@@ -78,19 +95,24 @@ const Visualizations = () => {
       // Axis labels
       svg
         .append("text")
-        .attr("x", width / 2)
-        .attr("y", height - 10)
+        .attr("x", updatedWidth / 2)
+        .attr("y", updatedHeight)
         .attr("text-anchor", "middle")
         .text("Location");
 
       svg
         .append("text")
         .attr("transform", "rotate(-90)")
-        .attr("x", -(height / 2) - 50)
+        .attr("x", -(updatedHeight / 2) - 50)
         .attr("y", 15)
         .attr("text-anchor", "middle")
         .text("Number of Crops");
     }
+  };
+
+  // Initial rendering
+  useEffect(() => {
+    renderChart();
   }, [locations]);
 
   return <div ref={visualizationRef}></div>;
